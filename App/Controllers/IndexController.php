d9718772fda12ba445ec3fe88f7e190ac924b9a8
unknown <agamenonjunior>
2021-10-13 01:03:59 -0300

    Implementando a camada View

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,12 +2,34 @@
 
 namespace App\Controllers;
 
+use stdClass;
+
 class indexController{
-    public function Index(){
-    echo "Chegamos ao indexControler action index";
+
+    private $view;
+
+    public function __construct()
+    {
+       $this->view = new \stdClass(); // cria um objeto vazio p/ criar atribuitos dentro das views 
     }
-    public function SobreNos(){
-        echo "Chegamos ao index";   
+
+    public function index(){
+        //isolando a camada view
+        $this->view->dados = array('titulo','descricao');
+        $this->render('index');
+    }
+    public function sobreNos(){
+        //isolando a camada view
+        $this->view->dados = array('titulo','descricao');        
+        $this->render('sobreNos');
+    }
+
+    public function render($view){
+        echo $classeAtual = get_class($this);
+        $classeAtual = str_replace("App\\Controllers\\", "",$classeAtual);
+        $classeAtual = strtolower(str_replace("Controller", "",$classeAtual));
+        echo "<hr>";      
+        require_once("../App/Views/".$classeAtual."/".$view.".phtml.");
     }
 }
 
--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
 require_once("../vendor/autoload.php");
 $route =  new  \App\Route;
-echo'ola mundo';
 
 print_r($route->getUrl());
 echo'<hr>';
-echo'<pre>';
-print_r($route->getRoutes());
-echo'</pre>';
 ?>
