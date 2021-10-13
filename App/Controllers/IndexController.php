2936a64873ac4b8ebd6b0184df38c7d7e29664cb
unknown <agamenonjunior>
2021-10-13 16:16:41 -0300

    Definindo e reutilizando Layout de página

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -11,12 +11,12 @@ class indexController extends Action{
     public function index(){
         //isolando a camada view
         $this->view->dados = array('titulo','descricao');
-        $this->render('index');
+        $this->render('index','layout1');
     }
     public function sobreNos(){
         //isolando a camada view
         $this->view->dados = array('titulo','descricao');        
-        $this->render('sobreNos');
+        $this->render('sobreNos','layout2');
     }
 
     
--- vendor/MF/Controller/Action.php
+++ vendor/MF/Controller/Action.php
@@ -3,7 +3,7 @@
 namespace MF\Controller;
 
 abstract class Action{
-    
+
     protected $view;
 
     public function __construct()
@@ -11,12 +11,23 @@ abstract class Action{
        $this->view = new \stdClass(); // cria um objeto vazio p/ criar atribuitos dentro das views 
     }
 
-    protected function render($view){
+    protected function render($view, $layout){
+        $this->view->page = $view;
+        if (file_exists("../App/Views/".$layout.".phtml")) {
+            require_once("../App/Views/".$layout.".phtml");  
+        }else{
+            $this->content();
+        }
+        
+        }
+
+    protected function content(){
         echo $classeAtual = get_class($this);
         $classeAtual = str_replace("App\\Controllers\\", "",$classeAtual);
         $classeAtual = strtolower(str_replace("Controller", "",$classeAtual));
         echo "<hr>";      
-        require_once("../App/Views/".$classeAtual."/".$view.".phtml.");
+        require_once("../App/Views/".$classeAtual."/".$this->view->page.".phtml.");
+    
     }
 }
 
