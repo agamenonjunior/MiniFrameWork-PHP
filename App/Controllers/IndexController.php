cb06d137e615bcbffb05d1938646172ec0aaa00a
unknown <agamenonjunior>
2021-10-12 23:51:00 -0300

    primeiro commit

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class indexController{
+    public function Index(){
+    echo "Chegamos ao indexControler action index";
+    }
+    public function SobreNos(){
+        echo "Chegamos ao index";   
+    }
+}
+
+
+
+
+?>
