81abd3bae8a40e1862621c44b30ef3c1c1f05f37
unknown <agamenonjunior>
2021-10-14 00:23:56 -0300

    Realizando a conexão com o banco de dados e Models

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,13 +4,21 @@ namespace App\Controllers;
 
 use stdClass;
 use MF\Controller\Action;
+use App\Connection;
+use App\Models\Produto;
 
 class indexController extends Action{
     
 
     public function index(){
         //isolando a camada view
-        $this->view->dados = array('titulo','descricao');
+        //$this->view->dados = array('titulo','descricao');
+        /**Instancia de conexao */
+        $conn = Connection::getDb();
+        /**Instancia de Modelo */
+        $produto = new Produto($conn);
+        $produtos = $produto->getProduto();
+        $this->view->dados = $produtos;
         $this->render('index','layout1');
     }
     public function sobreNos(){
---
+++ App/Models/Produto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+use PDO;
+
+class Produto{
+
+    protected $db;
+
+    public function __construct(\PDO $db)
+    {
+        $this->db = $db;
+    }
+    public function getProduto()
+    {
+        $query = "SELECT id,descricao,preco from tb_produtos";
+        return $this->db->query($query)->fetchAll();
+    }
+
+}
+
+
+?>
